8235fc900e2fb7f82e831e9cc417d5dda3075ee8
Sarthak Tripathi <str08925@gmail.com>
2025-06-26 17:50:49 +0530

    small change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/flat', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB Connection Error', err.message);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
-require('dotenv').config();
+require('dotenv').config();           // Load environment variables
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-connectDB(); 
-
 const app = express();
+connectDB();                         
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/listings', require('./routes/listing'));
 app.use('/auth', require('./routes/auth'));
 app.use('/seller', require('./routes/sellerAuth'));
 
+// Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
